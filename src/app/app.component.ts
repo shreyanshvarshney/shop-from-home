8403cca364c997a903d3991cce30675365bff992
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-04-16 23:45:08 +0530

    added ngOnDestroy() component life cycle hook to avoid memory leaks on client side from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { AngularFireDatabase } from '@angular/fire/database';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   item$: Observable<any[]>;
   items: any = [];
+  subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
     this.item$ = db.list('items').valueChanges();
-    this.item$.subscribe((data) => {
+    this.subscription = this.item$.subscribe((data) => {
       this.items = data;
       console.log(data);
     });
-    
+  }
+
+  // For avoiding memory leaks from the server
+  ngOnDestroy() {
+    this.subscription.unsubscribe();    
   }
 }
