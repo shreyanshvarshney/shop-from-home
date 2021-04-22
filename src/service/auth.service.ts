877d6d2b416049eb0bae5b796193735c9b73ccb5
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-04-23 00:28:28 +0530

    added custom auth service

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -9,20 +9,28 @@ import { Observable } from 'rxjs';
 })
 export class AuthService implements OnInit {
 
-  // user$: Observable<firebase.default.User>;
+  user$: Observable<firebase.default.User>;
 
-  constructor(private afAuth: AngularFireAuth) { }
-
-  ngOnInit() {
-    // this.user$ = this.afAuth.authState;
+  constructor(private afAuth: AngularFireAuth) {
+    this.user$ = afAuth.authState;
   }
 
+  ngOnInit() {}
+
   login() {
-    return this.afAuth.signInWithRedirect(new firebase.default.auth.GoogleAuthProvider());
+    // When I call signInWithRedirect -> my app is unloaded as the user is now on the Firebase auth page (look at the url).
+    // After that it redirects BACK to your application to the same route it was on when you called it
+    // So there is no point in trying to run code immediately AFTER calling signInWithRedirect, as that code is unreachable. 
+    // This is why there is the method getRedirectResult - which should be somewhere in your startup routine.
+    this.afAuth.signInWithRedirect(new firebase.default.auth.GoogleAuthProvider());
   }
 
   logout() {
     return this.afAuth.signOut();
+  }
+
+  getRedirectResult() {
+    return this.afAuth.getRedirectResult();
   }
 
   getAuthState() {
