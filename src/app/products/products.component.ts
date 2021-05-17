07d16da0a4be81fe4a8bcd4f85efee576bfaafd2
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-05-17 14:57:38 +0530

    added products display component with normal filtering using click event

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from './../../service/product.service';
+import { CategoryService } from './../../service/category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: any = [];
+  filteredProducts: any = [];
+  categories: any = [];
+  loading: boolean = true;
+
+  constructor(private productService: ProductService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.loadProducts();
+    this.loadCategories();
+  }
+
+  loadProducts() {
+    this.productService.getAll()
+    .subscribe((response) => {
+      this.loading = true;
+      this.filteredProducts = this.products = response.map((value => {
+        const key = value?.key;
+        const data: Object = value?.payload.val();
+
+        const obj = {
+          key: key,
+          ...data
+        };
+        return obj;
+      }));
+      this.loading = false;
+      // console.log(this.products);
+    });
+  }
+
+  loadCategories() {
+    this.categoryService.getAll()
+    .subscribe((response) => {
+      this.categories = response.map((value) => {
+        const key = value?.key;
+        const data: Object = value?.payload.val();
+
+        const obj = {
+          key: key,
+          ...data
+        };
+        return obj
+      });
+      // console.log(this.categories);
+    });
+  }
+
+  categoryFilter(category: string) {
+    this.filteredProducts = this.products.filter((value) => {
+      if(value?.category === category) return true;
+    });
+    // console.log(this.filteredProducts);
+  }
+
+  ngOnDestroy(): void {
   }
 
 }
