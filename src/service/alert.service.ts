4947630f34e2fb6d13f731991734e5e07ed91941
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-05-22 14:28:06 +0530

    swal fix

--- src/service/alert.service.ts
+++ src/service/alert.service.ts
@@ -13,7 +13,11 @@ export class AlertService {
       toast: true,
       position: 'top-end',
       showConfirmButton: false,
-      timer: 3000
+      timer: 2000,
+      customClass: {
+        container: 'swal-container-responsive',
+      },
+      
     });
     this.getAlert(toast, type, title);
   }
