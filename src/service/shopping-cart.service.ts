dddb83bcc12003612020a4b6deb662fe0404aaa3
shreyanshvarshney <varshney.shreyansh@gmail.com>
2021-06-04 04:04:36 +0530

    Code Refactoring in cart service

--- src/service/shopping-cart.service.ts
+++ src/service/shopping-cart.service.ts
@@ -37,34 +37,39 @@ export class ShoppingCartService {
     }
   }
 
-  // Model of cart: carts -> cardId -> items -> (productId -> product details, quantity)...
-  async addProductToCart(product: ProductDataModels) {
+  private async updateItemQuantity(product: ProductDataModels, change: number) {
     const cartId: string = await this.getOrCreateId();
     const item$ = this.getItemRef(cartId,product?.key);
     item$.snapshotChanges().pipe(
       take(1)
     ).subscribe((data) => {
       if(data.payload.exists()) {
-        item$.update({quantity: data?.payload.val()['quantity'] + 1});
+        item$.update({quantity: data?.payload.val()['quantity'] + change});
       } else {
         item$.set({product: product, quantity: 1});
       }
     });
   }
 
+  // Model of cart: carts -> cardId -> items -> (productId -> product details, quantity)...
+  async addProductToCart(product: ProductDataModels) {
+    this.updateItemQuantity(product,1);
+  }
+
   async removeProductFromCart(product: ProductDataModels) {
-    const cartId: string = await this.getOrCreateId();
-    const item$ = this.getItemRef(cartId,product?.key);
-    item$.snapshotChanges().pipe(
-      take(1)
-    ).subscribe((data) => {
-      // Will work with only statement this also.
-      // item$.update({quantity: data?.payload.val()['quantity'] - 1});
-      // But added this check for more assurance.
-      if(data.payload.exists()) {
-        item$.update({quantity: data?.payload.val()['quantity'] - 1});
-      }
-    });
+    this.updateItemQuantity(product,-1);
+    // const cartId: string = await this.getOrCreateId();
+    // const item$ = this.getItemRef(cartId,product?.key);
+    // item$.snapshotChanges().pipe(
+    //   take(1)
+    // ).subscribe((data) => {
+    //   // Will work with only statement this also.
+    //   // item$.update({quantity: data?.payload.val()['quantity'] - 1});
+    //   // But added this check for more assurance.
+    //   if(data.payload.exists()) {
+    //     item$.update({quantity: data?.payload.val()['quantity'] - 1});
+    //   }
+    // });
   }
 
 }
